43cbfead72f804874308c70665dae7cfda8ddf6f
lazarus2019 <thaisonbk2019@gmail.com>
2025-05-11 23:51:21 +0700

    feat(chessboard): handle piece move

--- src/modules/tutorial/components/chess-board.tsx
+++ src/modules/tutorial/components/chess-board.tsx
@@ -1,9 +1,10 @@
 import king from '@/assets/king.png'
 import pawn from '@/assets/pawn.png'
-import type { ReactElement } from 'react'
-import { pieces } from '../model/config'
+import { monitorForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter'
+import { type ReactElement, useEffect, useState } from 'react'
+import { initialPieces } from '../model/config'
 import { PieceType } from '../model/enum'
-import { isEqualCoord } from '../model/helper'
+import { canMove, isCoord, isEqualCoord, isPieceType } from '../model/helper'
 import type { Coord, PieceRecord } from '../types'
 import { Piece } from './piece'
 import { Square } from './square'
@@ -58,6 +59,49 @@ const renderSquares = (pieces: PieceRecord[]) => {
 }
 
 export const ChessBoard = () => {
+  const [pieces, setPieces] = useState<PieceRecord[]>(initialPieces)
+
+  useEffect(() => {
+    return monitorForElements({
+      onDrop: ({ source, location }) => {
+        const destination = location.current.dropTargets[0]
+
+        if (!destination) return // dropped outside of any drop targets
+
+        const destinationLocation = destination.data.location
+        const sourceLocation = source.data.location
+        const pieceType = source.data.pieceType
+
+        if (
+          // type guarding
+          !isCoord(destinationLocation) ||
+          !isCoord(sourceLocation) ||
+          !isPieceType(pieceType)
+        )
+          return
+
+        const piece = pieces.find((p) =>
+          isEqualCoord(p.location, sourceLocation)
+        )
+
+        if (!piece) return
+
+        const restOfPieces = pieces.filter((p) => p !== piece)
+
+        if (canMove(sourceLocation, destinationLocation, pieceType, pieces)) {
+          // moving the piece
+          setPieces([
+            {
+              type: piece.type,
+              location: destinationLocation
+            },
+            ...restOfPieces
+          ])
+        }
+      }
+    })
+  }, [pieces])
+
   return (
     <div className='grid grid-cols-8 grid-rows-8 w-[500px] h-[500px] border-3 border-[lightgray]'>
       {renderSquares(pieces)}
